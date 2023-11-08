2f2b63dd4b471dd594c00a7751328ab35b99c0cc
Shreyeska <silwalshreyeska@gmail.com>
2023-11-08 15:11:49 +0545

    Added pregnancy details

--- pregcalc/routes/index.js
+++ pregcalc/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Preg Calc' });
 });
+
+/* get about us page */
+router.get('/aboutus', function(req, res, next){
+  res.render('aboutus',{title: "About Us"})
+})
+
+/* get pregnancy calculator page */
+router.get('/pregcalc', function(req, res, next){
+  res.render('pregcalc',{title: "About Us"})
+})
 
 module.exports = router;
